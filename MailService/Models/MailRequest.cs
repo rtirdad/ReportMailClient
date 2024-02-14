f5d112f30c447e544c8b89fecaee8c5b25e819d9
rtirdad <94053847+rtirdad@users.noreply.github.com>
2024-02-14 11:19:10 +0100

    added model and settings

---
+++ MailService/Models/MailRequest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace SendEmail.Models
+{
+    public class MailRequest
+    {
+        public string ToEmail { get; set; }
+        public string ToDisplayName { get; set; }
+        public string FromDisplayName { get; set; }
+
+        //public string FromMail { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        //public IFormFile? Attachments { get; set; }
+        
+        public string JsonData { get; set; }
+
+        public JsonElement GetJsonDataAsJsonElement() => JsonDocument.Parse(JsonData).RootElement;
+    }
+}
