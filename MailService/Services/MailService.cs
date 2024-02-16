924ed073f0d147373f531555aca1821b06e6c731
rtirdad <94053847+rtirdad@users.noreply.github.com>
2024-02-16 15:27:01 +0100

    .

--- MailService/Services/MailService.cs
+++ MailService/Services/MailService.cs
@@ -32,35 +32,6 @@ namespace MailService.Services
             email.Subject = mailrequest.Subject;
             var builder = new BodyBuilder();
 
-            /*if (mailrequest.Attachments != null)
-            {
-                /*byte[] fileBytes;
-                foreach (var file in mailrequest.Attachments)
-                {
-                    if (file.Length > 0)
-                    {
-                        using (var ms = new MemoryStream())
-                        {
-                            file.CopyTo(ms);
-                            fileBytes = ms.ToArray();
-                        }
-                        builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
-                    }
-                }
-                using (var ms = new MemoryStream())
-                {
-                    await mailrequest.Attachments.CopyToAsync(ms);
-                    ms.Seek(0, SeekOrigin.Begin);
-                    var attachment = new MimePart(mailrequest.Attachments.ContentType)
-                    {
-                        Content = new MimeContent(ms),
-                        ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
-                        ContentTransferEncoding = ContentEncoding.Base64,
-                        FileName = mailrequest.Attachments.FileName
-                    };
-                    builder.Attachments.Add(attachment);
-                }
-            }*/
             builder.HtmlBody = mailrequest.Body;
             email.Body = builder.ToMessageBody();
             using var smtp = new MailKit.Net.Smtp.SmtpClient();
---
+++ ReportMailConsole/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // 1. Retrieve the report as an attachment
+        var reportClient = new HttpClient();
+        var reportRequest = new
+        {
+            format = "Pdf",
+            template = "Letter1",
+            data = new
+            {
+                client = new
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    PostCode = "1234AB",
+                    CompanyName = "Apple",
+                    Address = "Apple Street 123",
+                    Title = "Mr."
+                }
+            }
+        };
+
+        var reportResponse = await reportClient.PostAsJsonAsync("https://localhost:7251/report", reportRequest);
+        byte[] attachment = null;
+
+        if (reportResponse.IsSuccessStatusCode)
+        {
+            attachment = await reportResponse.Content.ReadAsByteArrayAsync();
+        }
+        else
+        {
+            Console.WriteLine($"Failed to generate report. Status code: {reportResponse.StatusCode}");
+            return;
+        }
+
+        // 2. Send email with the report attachment
+        var emailClient = new HttpClient();
+        var mailRequest = new
+        {
+            ToEmail = "ronat20003@gmail.com",
+            ToDisplayName = "r",
+            FromDisplayName = "Your Name",
+            FromMail = "ronat20003@gmail.com",
+            Subject = "Report Attached",
+            Body = "Please find the attached report."
+        };
+
+        var formData = new MultipartFormDataContent();
+        formData.Add(new ByteArrayContent(attachment), "attachment", "report.pdf");
+        formData.Add(JsonContent.Create(mailRequest), "mailRequest");
+
+        var mailResponse = await emailClient.PostAsync("https://localhost:7154/Email/Send", formData);
+
+        if (mailResponse.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Email sent successfully!");
+        }
+        else
+        {
+            Console.WriteLine($"Failed to send email. Status code: {mailResponse.StatusCode}");
+        }
+    }
+}
