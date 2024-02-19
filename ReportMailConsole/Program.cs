c135844bfae3b1dbfa9323f7fa41babec64354dc
rtirdad <94053847+rtirdad@users.noreply.github.com>
2024-02-19 12:53:18 +0100

    dont send

--- ReportMailConsole/Program.cs
+++ ReportMailConsole/Program.cs
@@ -1,33 +1,33 @@
-﻿//using GemBox.Document;
-using GemBox.Document;
+﻿using GemBox.Document;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
-
 
 ComponentInfo.SetLicense("FREE-LIMITED-KEY");
 
 var reportClient = new HttpClient();
-var reportJsonContent = new
+var JsonData = @"
 {
-    format = "pdf",
-    template = "Letter1",
-    data = new
-    {
-        client = new
-        {
-            FirstName = "John",
-            LastName = "Smith",
-            PostCode = "1234AB",
-            CompanyName = "Apple",
-            Address = "Apple Street 123",
-            Title = "Mr."
-        }
+  ""format"": ""html"",
+  ""template"": ""Letter1"",
+  ""data"": 
+  { 
+    ""client"": {
+      ""FirstName"": ""John"",
+      ""LastName"": ""Smith"",
+      ""PostCode"": ""1234AB"",
+      ""CompanyName"": ""Apple"",
+      ""Address"": ""Apple Street 123"",
+      ""Title"": ""Mr.""
     }
-};
+  }
+}";
+var reportJsonContent = JsonDocument.Parse(JsonData);
+
 var reportResult = await reportClient.PostAsJsonAsync("https://localhost:7251/report", reportJsonContent);
 
 string reportFilePath = Path.GetTempFileName();
@@ -40,17 +40,16 @@ using (var fileStream = File.Create(reportFilePath))
 var emailClient = new HttpClient();
 var emailContent = new
 {
-    toEmail = "Ronat20003@gmail.com",
+    toEmail = "ronat20003@gmail.com",
     toDisplayName = "Rona",
     fromDisplayName = "R",
     fromMail = "ronat20003@gmail.com",
-    subject = "Console App Email with Report",
-    body = "Hi there, please find attached the report.",
+    subject = "Console App Email with Report attachment",
+    body = "Email with attachment",
     attachmentPath = reportFilePath
 };
 
 var emailResult = await emailClient.PostAsJsonAsync("https://localhost:7154/Email/SendWithAttachment", emailContent);
-
 if (emailResult.IsSuccessStatusCode)
 {
     Console.WriteLine("Email sent successfully!");
